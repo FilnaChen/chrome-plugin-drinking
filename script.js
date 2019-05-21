755ee878b7a4fca5715e9c30505be3b4ec5b7ec6
陈月娜 <1121776448@qq.com>
2019-05-21 19:21:12 +0800

    fix(plugin): 修改插件的时间和样式

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+const wrap = document.createElement('div');
 const pic = document.createElement('img');
 const remind = document.createElement('p');
 const recieve = document.createElement('div');
 const body = document.getElementsByTagName('body')[0];
-body.appendChild(pic);
-body.appendChild(remind);
-body.appendChild(recieve);
+wrap.appendChild(remind);
+wrap.appendChild(pic);
+wrap.appendChild(recieve);
+body.appendChild(wrap);
+wrap.style.cssText = "width: 100vw;height: 100vh;position: fixed;left: 0;top: 0;display: flex; justify-content: center;align-items: center;background-color: rgba(0, 0, 0, 0.5);flex-direction: column;display: none";
+pic.style.cssText = "width: 300px;height: auto";
+remind.style.cssText = "color: pink";
+recieve.style.cssText = "color: pink;margin-top: 20px";
 const picArr = [
-    './images/pic2.jpg',
-    './images/pic3.jpg',
-    './images/pic4.jpg',
-    './images/pic5.jpg',
-    './images/pic6.jpg',
-    './images/pic7.jpg',
-    './images/pic8.jpg',
-    './images/pic9.jpg',
-    './images/pic10.jpg'
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic2.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic3.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic4.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic5.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic6.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic7.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic8.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic9.jpg?raw=true',
+    'https://github.com/FilnaChen/chrome-plugin-drinking/blob/master/images/pic10.jpg?raw=true',
 ];
 window.onload = function() {
     setInterval(function() {
         const now = new Date();
         const minutes = now.getMinutes();
         const seconds = now.getSeconds();
-        if (seconds === 3 || seconds === 13 || seconds === 23 || seconds === 33 || seconds === 43 || seconds === 53) {
-            remind.innerText = "岐岐让你喝水啦";
+        if (minutes ===0 && seconds === 0) {
             pic.src = picArr[Math.floor(Math.random() * picArr.length)];
-            recieve.innerText = "知道啦岐岐";
+            pic.onload = function() {
+                wrap.style.display = "flex";
+                remind.innerText = "岐岐让你喝水啦";
+                recieve.innerText = "知道啦岐岐";
+            };
         }
     }, 1000);
 };
 recieve.onclick = function() {
-    remind.innerText = '';
-    pic.src = '';
-    recieve.innerText = '';
+    wrap.style.display = "none";
 };
